52e377c32236f391f60757511737ea173285a0bf
Shuzaiku <69157153+Shuzaiku@users.noreply.github.com>
2022-10-24 11:26:47 -0400

    Create harp_tuning.py

---
+++ CCC 2022/harp_tuning.py
@@ -0,0 +1,17 @@
+# 15 / 15 score submission
+instructions = input()
+start_idx = 0
+
+actions = {
+    '+': " tighten ",
+    '-': " loosen "
+}
+
+for i in range(1, len(instructions)):
+    if (instructions[i-1].isdigit() and not instructions[i].isdigit()) or i == len(instructions)-1:
+        if i == len(instructions)-1:
+            i += 1
+        subbed = instructions[start_idx:i]
+        symbol = subbed.find('+') != -1 and '+' or '-'
+        print(subbed.replace(symbol, actions[symbol]))
+        start_idx = i
